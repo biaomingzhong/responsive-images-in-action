5e6ed4809ca69bf8a1fda94530b54a303c2c187e
Amos <biaomingzhong@gmail.com>
2017-05-04 13:52:21 +0800

    添加初步媒体查询

--- http.js
+++ http.js
@@ -1,10 +1,11 @@
-"use strict";
-const express = require("express");
-const favicon = require("serve-favicon");
-const compression = require("compression");
-let app = express();
+const express = require("express")
+const favicon = require("serve-favicon")
+const compression = require("compression")
+const path = require("path")
+let app = express()
 
 // Run static server
-app.use(compression());
-app.use(express.static(__dirname));
-app.listen(8080);
+app.use(compression())
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
+app.use(express.static(__dirname))
+app.listen(8080)
